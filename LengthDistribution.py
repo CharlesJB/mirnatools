4ced06357ccff275439f7ffa90007ba32540d6b5
CharlesJB <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2012-07-25 13:37:49 -0400

    Copied from NGS-Pipelines

---
+++ LengthDistribution.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+# encoding: utf-8
+# author: Charles Joly Beauparlant
+# 2012-07-23
+
+"""
+This script calculates the distribution of sequence's length from a fasta file
+Usage:
+cat joe.fasta | ./LengthDistribution.py <maxLength> > distribution.txt
+maxLength: Maximum sequence length to compute
+"""
+
+class DistributionCalculator:
+        def __init__(self, maxLength):
+                self.maxLength = maxLength
+                self.clear()
+
+        def clear(self):
+                self.distribution = [0] * self.maxLength
+
+        def processSequence(self, sequence, count):
+                length = len(sequence)
+                if length <= self.maxLength:
+                        self.distribution[length] += count
+
+        def printResults(self):
+		# Calculate total number of sequences
+		total = 0
+		for i in range(self.maxLength):
+			total += self.distribution[i]
+		# Print percentage of total for each lenght
+                for i in range(self.maxLength):
+			percent = self.distribution[i] / float(total) * 100
+                        print str(i) + "\t" + str(percent)
+	
+
+import sys
+
+if __name__=="__main__":
+        if len(sys.argv)!=2:
+                print __doc__
+                sys.exit(1)
+
+        maxLength=int(sys.argv[1])
+        distributionCalculator = DistributionCalculator(maxLength)
+
+        i =0
+        for line in sys.stdin:
+		if i==0:
+			count = int(line.split()[3])
+                if i==1:
+                        sequence = line.strip()
+                        distributionCalculator.processSequence(sequence, count)
+
+                i+=1
+
+                if i==2:
+                        i=0
+
+        distributionCalculator.printResults()
+
