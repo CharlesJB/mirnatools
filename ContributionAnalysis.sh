bd6bfc0dcc8878fa7632889e0dd0b776972d18ea
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2012-09-26 15:32:56 -0400

    Minor cosmetic changes

--- ContributionAnalysis.sh
+++ ContributionAnalysis.sh
@@ -7,7 +7,7 @@ Usage() {
 	echo "idToRemove: List of IDs that match known miRNA. We need to remove them from the results."
 	echo "usableSequences: List of valid RNA sequence (after trimming)."
 	echo "scriptsPath: path containing the miRNA-Tools git clone."
-	echo "             (https://github.com/CharlesJB/miRNA-Tools)"
+	echo "			 (https://github.com/CharlesJB/miRNA-Tools)"
 }
 
 ValidateFile() {
@@ -24,17 +24,17 @@ usableSequences=$2
 scriptsPath=$3
 
 # 1. Calculate total number of sequences
-echo "    Caculating total number of usable sequences."
+echo "	Caculating total number of usable sequences."
 numberOfSequences=$(grep '>' $usableSequences | awk '{ SUM += $4 } END { print SUM }')
-echo "        $numberOfSequences sequences."
+echo "		$numberOfSequences sequences."
 
 # 2. Calulate relative contribution based on total number of sequences
-echo "    Calculating relative contribution."
+echo "	Calculating relative contribution."
 uniqueFastaOutput=$baseRef"_blast_ID_unique.fa"
 relativeOutput="lengthDist_"$baseRef"_unique_relative.txt"
 cat $uniqueFastaOutput | $scriptsPath/LengthDistribution.py 60 $numberOfSequences > $relativeOutput
 
 # 3. Plot the relative distribution
-echo "    Plotting the relative distribution."
+echo "	Plotting the relative distribution."
 cat $usableSequences | $scriptsPath/LengthDistribution.py 60 > lengthDist.tmp
 Rscript $scriptsPath/PlotRelativeLengthDistribution.R $relativeOutput lengthDist.tmp $baseRef &> /dev/null
