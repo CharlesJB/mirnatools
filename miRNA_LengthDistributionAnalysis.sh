e25421b84a6a3a990b0e21b31ea5b22bc0dc465c
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2012-11-21 15:48:25 -0500

    miRNA_LengthDistributionAnalysis.sh: graph were not outputed correctly
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

--- miRNA_LengthDistributionAnalysis.sh
+++ miRNA_LengthDistributionAnalysis.sh
@@ -9,13 +9,12 @@ cut -f2- "mature_"$id"_noMatchLength_ID.txt" | sed 's/\t/\n/g' | sort | uniq > $
 cut -f2- "mature_"$id"_noMatchMismatches_ID.txt" | sed 's/\t/\n/g' | sort | uniq > $id"_noMatchMismatches_ID.tmp"
 cut -f2- "mature_"$id"_perfectSeqID.txt" | sed 's/\t/\n/g' | sort | uniq > $id"_perfectSeqID.tmp"
 
-for file in $(ls | grep 'ID\.tmp')
+for file in $(ls | grep '_ID\.tmp')
 do
 	name=${file%_*}
-	name=${nameEnd%.*}
-	name="mature_"$id$name".fa"
+	name=$id$name".fa"
 	$path/getFasta.py $file $id"_UsableSeq.fasta" > $name
-	lengthDist="lengthDist"${name%.*}".txt"
+	lengthDist="lengthDist_"${name%.*}".txt"
 	cat $name | $path/LengthDistribution.py 60 > $lengthDist
 	Rscript $path/PlotLengthDistribution.R $lengthDist ${file%.*}
 done
