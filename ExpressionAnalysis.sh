f3c7391e4c467f138c51183a3d94a7778ad7c18b
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2012-12-12 15:25:09 -0500

    ExpressionAnalysis.sh: New version now produce 2 types of graph (pie chart and miRNA expression levels)
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

--- ExpressionAnalysis.sh
+++ ExpressionAnalysis.sh
@@ -2,32 +2,36 @@
 
 Usage() {
 	echo "Usage:"
-	echo "pieChartProducter.sh <usableSequences> <miRNApath> <toGrep>"
-	echo "    usableSequences: list of usable sequences obtained during raw sequence filtering"
+	echo "ExpressionAnalysis.sh <miRNApath> <toGrep> <scriptsPath>"
 	echo "    miRNApath: path to miRNA analysis (relative)"
 	echo "    toGrep: extra filter for miRNA analysis files"
 	echo "    scriptsPath: path containing the miRNA-Tools git clone."
 	echo "		     ( https://github.com/CharlesJB/miRNA-Tools )"
 }
 
-usableSequences=$1
-path=$2
-toGrep=$3
-scriptsPath=$4
+path=$1
+toGrep=$2
+scriptsPath=$3
 
 mkdir -p ExpressionLevels
 
+# Get the sum of all counts from GenomeMapping miRNA analysis
 file=$path/$(ls $path | grep mature | grep combined | grep $toGrep | head -n1)
+count=$(awk '{ sum += $2 } END {printf "%2.2f\n", sum}' $file)
 
-count=$(grep '>' $usableSequences | cut -f4 -d' ' | awk '{ sum += $1 } END {print sum/1000000}')
-toAwk="awk '{ printf \"%4.3f\\n\", (\$2/$count) }' $file > ExpressionLevels/percent.tmp"
+# Convert the absolute sequence count by the sum of all absolute count
+toAwk="awk '{ printf \"%4.8f\\n\", (\$2/$count) }' $file > ExpressionLevels/percent.tmp"
 eval $toAwk
+
+# Merge the percentage with the correct names and clean up intermediate files
 cut -f1 $file > ExpressionLevels/names.tmp
 output="ExpressionLevels/mature_combinedMatches_relativeCount_"$toGrep".txt"
 paste ExpressionLevels/names.tmp ExpressionLevels/percent.tmp | sort -k2 -nr > $output
 rm ExpressionLevels/*.tmp
 
+# Generate the data file to be used by the graph production scripts
 distributionFile=${output%.*}"_distribution.txt"
+#   pie chart graph
 sort -k2 -nr $output | head -n15 > $distributionFile
 count=$(wc -l $distributionFile | awk '{print $1}')
 if [ "$count" -ge 15 ]
@@ -38,3 +42,10 @@ fi
 pieChartBasename="ExpressionLevels/"$toGrep"_pieChart"
 Rscript $scriptsPath/percent_pie_chart.R jpeg $distributionFile $pieChartBasename | sed '/null device/d' | sed -e '/          1/d'
 Rscript $scriptsPath/percent_pie_chart.R tiff $distributionFile $pieChartBasename | sed '/null device/d' | sed -e '/          1/d'
+
+# miRNA expression level graph
+absoluteCountFile="ExpressionLevels/"$(basename $file)
+expressionBasename="ExpressionLevels/"$toGrep"_expressionLevels"
+sort -k2 -nr $file > $absoluteCountFile
+Rscript $scriptsPath/profiling.R jpeg $absoluteCountFile $expressionBasename | sed '/null device/d' | sed -e '/          1/d' 
+Rscript $scriptsPath/profiling.R tiff $absoluteCountFile $expressionBasename | sed '/null device/d' | sed -e '/          1/d'
