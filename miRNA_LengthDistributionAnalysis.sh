160e9c734375620f56d89ace30903a2b9f9a4f68
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2012-12-14 11:34:15 -0500

    miRNA_LengthDistributionAnalysis.sh: New version that is more compact
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

--- miRNA_LengthDistributionAnalysis.sh
+++ miRNA_LengthDistributionAnalysis.sh
@@ -3,18 +3,17 @@
 id=$1
 path=$2
 
-cut -f2- "mature_"$id"_looseSeqID.txt" | sed 's/\t/\n/g' | sort | uniq > $id"_looseSeqID.tmp"
-cut -f2- "mature_"$id"_noMatchGaps_ID.txt" | sed 's/\t/\n/g' | sort | uniq > $id"_noMatchGaps_ID.tmp"
-cut -f2- "mature_"$id"_noMatchLength_ID.txt" | sed 's/\t/\n/g' | sort | uniq > $id"_noMatchLength_ID.tmp"
-cut -f2- "mature_"$id"_noMatchMismatches_ID.txt" | sed 's/\t/\n/g' | sort | uniq > $id"_noMatchMismatches_ID.tmp"
-cut -f2- "mature_"$id"_perfectSeqID.txt" | sed 's/\t/\n/g' | sort | uniq > $id"_perfectSeqID.tmp"
-
-for file in $(ls | grep '_ID\.tmp')
+echo id:$id
+for file in $(ls | grep $id | grep '_ID' | grep txt) 
 do
-	name=${file%_*}
-	name=$id$name".fa"
+	echo file:$file
+	tmpfile=${file%.*}".tmp"
+	cut -f2- $file | sed 's/\t/\n/g' | sort | uniq > $tmpfile
+	name=${file%_*}".fa"
 	$path/getFasta.py $file $id"_UsableSeq.fasta" > $name
 	lengthDist="lengthDist_"${name%.*}".txt"
 	cat $name | $path/LengthDistribution.py 60 > $lengthDist
 	Rscript $path/PlotLengthDistribution.R $lengthDist ${file%.*}
 done
+
+rm *.tmp
