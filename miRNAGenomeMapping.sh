4fe3e36619672397f62ddff656ee67625db2d89b
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2012-12-21 11:33:02 -0500

    miRNAGenomeMapping.sh: Major changes in the code to deal with new Drivers and Utility functions
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

--- miRNAGenomeMapping.sh
+++ miRNAGenomeMapping.sh
@@ -3,58 +3,61 @@
 Usage() {
 	echo ""
 	echo "Usage:"
-	echo "GenomeMapping.sh <referenceData> <usableSequences> <scriptsPath>"
+	echo "GenomeMapping.sh <referenceData> <usableSequences>"
 	echo "referenceData: For the blast analysis. Must be in fasta format."
 	echo "usableSequences: List of valid RNA sequence (after trimming)."
-	echo "scriptsPath: path containing the miRNA-Tools git clone."
-	echo "			 (https://github.com/CharlesJB/miRNA-Tools)"
+	echo ""
+	echo "Note: You must set the MIRNA_TOOLS_PATH environment variable to miRNA-Tools folder"
+	echo "      This can be done either manually or by using LoadModules, i.e.:"
+	echo "      source ~/git-clones/miRNA-Tools LoadModules"
+	echo ""
 }
 
-ValidateFile() {
-	fileToCheck=$1
-	if [ ! -e "$fileToCheck" ]
-	then
-		echo "Invalid file: $fileToCheck"
-		Usage
-		exit
-	fi
-}
+# Set variables
+source $MIRNA_TOOLS_PATH/Utility/utility.sh
+EXPECTED_ARGS=2
 
+# Load parameters
 reference=$1
 usableSequences=$2
-scriptsPath=$3
 
-ValidateFile $reference
-ValidateFile $usableSequences
-ValidateFile $scriptsPath/BlastAnalysis.sh
-ValidateFile $scriptsPath/LengthDistributionAnalysis.sh
+if [ $# -eq $EXPECTED_ARGS ]
+then
+	# Validate files
+	ValidateFile $reference
+	ValidateFile $usableSequences
+	ValidateFile $MIRNA_TOOLS_PATH/BlastAnalysis.sh
+	ValidateFile $MIRNA_TOOLS_PATH/miRNA_LengthDistributionAnalysis.sh
+	ValidateFile $MIRNA_TOOLS_PATH/miRNAContributionAnalysis.sh
 
-# 1. Blast
-echo ""
-echo "Beginning blast analysis..."
-$scriptsPath/BlastAnalysis.sh $reference $usableSequences "0.1"
-echo "Done!"
+	# Do the actual analysis
+	echo ""
+	echo "Beginning blast analysis..."
+	BlastAnalysis.sh $reference $usableSequences "0.1"
+	echo "Done!"
 
-blastOutput=$(basename ${reference%.*})".out"
+	blastOutput=$(basename ${reference%.*})".out"
 
-echo ""
-echo "Launching Preethi's algorithm..."
-$scriptsPath/preethi_blast_analysis.py $blastOutput ${blastOutput%.*}
-echo "Done!"
+	echo ""
+	echo "Launching Preethi's algorithm..."
+	preethi_blast_analysis.py $blastOutput ${blastOutput%.*}
+	echo "Done!"
 
-echo ""
-echo "Beginning length distribution analysis..."
-id=$(echo $(basename $usableSequences) | sed 's/_UsableSeq.fasta//g')
-$scriptsPath/miRNA_LengthDistributionAnalysis.sh $id $scriptsPath
-echo "Done!"
+	echo ""
+	echo "Beginning length distribution analysis..."
+	id=$(echo $(basename $usableSequences) | sed 's/_UsableSeq.fasta//g')
+	miRNA_LengthDistributionAnalysis.sh $id $scriptsPath
+	echo "Done!"
 
-# 3. Contribution analysis
-echo ""
-echo "Beginning contribution analysis..."
-baseRef=$(basename ${reference%.*})
-$scriptsPath/miRNAContributionAnalysis.sh $id $usableSequences $scriptsPath
-echo "Done!"
+	echo ""
+	echo "Beginning contribution analysis..."
+	baseRef=$(basename ${reference%.*})
+	miRNAContributionAnalysis.sh $id $usableSequences $scriptsPath
+	echo "Done!"
 
-# 5. Clean up
-rm -f *.tmp
-echo ""
+	# Clean up
+	rm -f *.tmp
+	echo ""
+else
+	Usage
+fi
