035560194dc14e4ad28943dc5a73fc0cb23692bd
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2015-06-03 13:04:04 -0400

    Package skeleton
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

---
+++ R/miRNA-Tools-package.R
@@ -0,0 +1,8 @@
+#' miRNA-Tools: A package to manage miRNA sequencing data
+#'
+#' Description: This package contains multiple fonctions to allow the analysis of
+#'    deep sequencing miRNA datasets.
+#
+#' @docType package
+#' @name miRNA-Tools
+NULL
---
+++ README.md
@@ -0,0 +1,10 @@
+# mirnatools 
+
+A package to manage miRNA sequencing data.
+
+## Installation
+
+```
+require(devtools)
+devtools::install_github("CharlesJB/mirnatools")
+```
